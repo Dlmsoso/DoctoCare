348397a8006cd4d8473163a1c3de3588c42c2c00
Maxime ULMANN <maxime.ulmann@epita.fr>
2023-06-05 16:00:55 +0200

    Change archi

--- docto/android/app/src/main/kotlin/com/example/docto/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.docto
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- docto/lib/provider/profileProvider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class ProfileProvider with ChangeNotifier, DiagnosticableTreeMixin {
-  String? name;
-  List<String>? _doctorList = ["Medecin 1", "Medecin 2", "Medecin 3"];
-
-  List<String> get doctorList => _doctorList!;
-}
